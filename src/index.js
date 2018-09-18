050d3ae4e653458bb138191c7af6285653d6e57f
Alina Lutsevich <alinkaluya@gmail.com>
2018-09-18 17:43:40 +0300

    Two files were merged

--- src/index.js
+++ src/index.js
@@ -1,37 +1,55 @@
-const {isEqualArrays, isFakeTriangle, sumArray} = require('./modules');
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
+    function isEqualArrays(arr1, arr2) {
+        const objMap = {};
 
-    let loveTriangles = [];
-    for (let i = 0; i < preferences.length; i++) {
+        arr1.forEach((e1) => arr2.forEach((e2) => {
+            if (e1 === e2) {
+                    objMap[e1] = objMap[e1] + 1 || 1;
+                }
+            }
+        ));
 
-        let keys = [];
-        let values = [];
-        let key = i;
-        for (let j = 0; j < 3; j++) {
-            keys[j] = key + 1;
-            values[j] = preferences[key];
-            key = preferences[key] - 1;
-        }
+        return Object.keys(objMap).map(e => Number(e)).length === arr1.length;
+    }
 
-        if ((sumArray(keys) === sumArray(values)) && (i + 1 === values[2]) && !isFakeTriangle(values)) {
-            let exists = false;
-            for (let k = 0; k < loveTriangles.length; k++) {
-                if (isEqualArrays(loveTriangles[k], values)) {
-                    exists = true;
+    function isFakeTriangle(arr) {
+        return arr[0] === arr[1];
+    }
+
+    function sumArray(arr) {
+        return arr.reduce((x, y) => x + y);
+    }
+
+
+            let loveTriangles = [];
+            for (let i = 0; i < preferences.length; i++) {
+
+                let keys = [];
+                let values = [];
+                let key = i;
+                for (let j = 0; j < 3; j++) {
+                    keys[j] = key + 1;
+                    values[j] = preferences[key];
+                    key = preferences[key] - 1;
+                }
+
+                if ((sumArray(keys) === sumArray(values)) && (i + 1 === values[2]) && !isFakeTriangle(values)) {
+                    let exists = false;
+                    for (let k = 0; k < loveTriangles.length; k++) {
+                        if (isEqualArrays(loveTriangles[k], values)) {
+                            exists = true;
+                        }
+                    }
+
+                    if (!exists) {
+                        loveTriangles.push(values);
+                    }
                 }
             }
 
-            if (!exists) {
-                loveTriangles.push(values);
-            }
-        }
-    }
-
-    return loveTriangles.length;
+            return loveTriangles.length;
+  
 
 };
 
